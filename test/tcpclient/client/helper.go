55712169d1a7a37e00df885dea9acd637024818e
unknown <lgbya>
2026-06-28 22:33:16 +0800

    测试随机职业

--- test/tcpclient/client/helper.go
+++ test/tcpclient/client/helper.go
@@ -2,31 +2,12 @@ package client
 
 import (
 	"math/rand"
-	"sync"
-	"time"
 )
-
-var (
-	r    *rand.Rand
-	mux  sync.Mutex
-	once sync.Once
-)
-
-func init() {
-	once.Do(func() {
-		r = rand.New(rand.NewSource(time.Now().UnixNano()))
-	})
-}
 
 func Rand(min, max int) int {
 	if min >= max {
 		return min
 	}
-	mux.Lock()
-	defer mux.Unlock()
-	// 如果 r 为 nil，再次初始化（安全）
-	if r == nil {
-		r = rand.New(rand.NewSource(time.Now().UnixNano()))
-	}
-	return r.Intn(max-min+1) + min
+	// 直接使用全局随机数（Go 1.20+ 自动种子化，内部已加锁，并发安全）
+	return rand.Intn(max-min+1) + min
 }
